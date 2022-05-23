20f7ace8f42d726b2c052f8d701f4a59dd5ba0be
ArifulSikder <67917301+ArifulSikder@users.noreply.github.com>
2022-05-23 15:56:18 +0600

    product discount fixed

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -122,9 +122,12 @@
                                                     </a>
                                                     <div class="product-label-group">
                                                         <label class="product-label label-new">নতুন</label>
-                                                        <label
-                                                            class="product-label label-sale">{{ $product->product_discount }}%
-                                                            off</label>
+                                                        @if ($product->product_discount > 0)
+                                                            <label class="product-label label-sale">
+                                                                {{ $product->product_discount }}% off
+                                                            </label>
+                                                        @endif
+
                                                     </div>
                                                     <div class="product-action-vertical">
                                                         <button class="btn-product-icon ViewProduct"
